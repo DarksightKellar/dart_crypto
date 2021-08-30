934b9b68c35c02d6dc1eac8f3110efe3a0a0a566
Kelvin <kellar.base09@gmail.com>
2021-08-30 11:58:47 +0100

    Add .fromMnemonic constructor

--- lib/src/seed/seed.dart
+++ lib/src/seed/seed.dart
@@ -4,7 +4,15 @@ import 'package:bip39/bip39.dart' as bip39;
 
 class Seed {
 
-  final String _mnemonic = bip39.generateMnemonic();
+  late final String _mnemonic;
+
+  Seed() {
+     _mnemonic = bip39.generateMnemonic();
+  }
+
+  Seed.fromMnemonic(Strin mnemonic) {
+     _mnemonic = mnemonic;
+  }
 
   String get mnemonic => _mnemonic;
   Uint8List get seed => bip39.mnemonicToSeed(_mnemonic);
